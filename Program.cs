688f8c0b5b28e46859a836ec7520cd0821f56e85
Chuong <chuonglepro1994@gmail.com>
2023-09-18 18:04:24 -0400

    Add more features for the project

--- Program.cs
+++ Program.cs
@@ -17,18 +17,66 @@ namespace OOP
             // Print the details of each person in the list
             PrintPeople(people);
 
-            // Wait for user input before exiting
-            Console.ReadKey();
+            // Implement additional features
+            while (true)
+            {
+                Console.WriteLine("\nAdditional Features:");
+                Console.WriteLine("1. Sort by Age");
+                Console.WriteLine("2. Sort by First Name");
+                Console.WriteLine("3. Sort by Last Name");
+                Console.WriteLine("4. Search by First Name");
+                Console.WriteLine("5. Search by Last Name");
+                Console.WriteLine("6. Search by Occupation");
+                Console.WriteLine("7. Export to CSV");
+                Console.WriteLine("8. Exit");
+
+                int choice = GetChoice(1, 8);
+
+                switch (choice)
+                {
+                    case 1:
+                        people = SortByAge(people);
+                        PrintPeople(people);
+                        break;
+                    case 2:
+                        people = SortByFirstName(people);
+                        PrintPeople(people);
+                        break;
+                    case 3:
+                        people = SortByLastName(people);
+                        PrintPeople(people);
+                        break;
+                    case 4:
+                        Console.Write("Enter First Name to search: ");
+                        string firstNameToSearch = Console.ReadLine();
+                        SearchByFirstName(people, firstNameToSearch);
+                        break;
+                    case 5:
+                        Console.Write("Enter Last Name to search: ");
+                        string lastNameToSearch = Console.ReadLine();
+                        SearchByLastName(people, lastNameToSearch);
+                        break;
+                    case 6:
+                        Console.Write("Enter Occupation to search: ");
+                        string occupationToSearch = Console.ReadLine();
+                        SearchByOccupation(people, occupationToSearch);
+                        break;
+                    case 7:
+                        ExportToCSV(people);
+                        break;
+                    case 8:
+                        Console.WriteLine("Exiting the program.");
+                        return;
+                }
+            }
         }
 
-        // Function to read lines from a file
         static string[] Readfile(string filename)
         {
             string[] lines = System.IO.File.ReadAllLines(filename);
             return lines;
         }
 
-        // Function to convert CSV data into a list of Person objects
         static List<Person> getPeople(string[] file)
         {
             Dictionary<int, List<string>> file_items = new Dictionary<int, List<string>>();
@@ -75,7 +123,6 @@ namespace OOP
             return people;
         }
 
-        // Function to split a CSV line into individual items
         static List<string> GetItems(string line)
         {
             string current_word = "";
@@ -104,13 +151,103 @@ namespace OOP
             return items;
         }
 
-        // Function to print details of people
         static void PrintPeople(List<Person> people)
         {
+            Console.WriteLine("\nList of People:");
             foreach (Person p in people)
             {
                 Console.WriteLine($"{p._FirstName} {p._LastName} is {p._Age.ToString()} years old and works as a {p._Occupation}");
             }
+        }
+
+        static List<Person> SortByAge(List<Person> people)
+        {
+            return people.OrderBy(p => p._Age).ToList();
+        }
+
+        static List<Person> SortByFirstName(List<Person> people)
+        {
+            return people.OrderBy(p => p._FirstName).ToList();
+        }
+
+        static List<Person> SortByLastName(List<Person> people)
+        {
+            return people.OrderBy(p => p._LastName).ToList();
+        }
+
+        static void SearchByFirstName(List<Person> people, string firstName)
+        {
+            var results = people.Where(p => p._FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine($"No matching records found for first name: {firstName}");
+            }
+            else
+            {
+                Console.WriteLine($"\nSearch Results for First Name: {firstName}");
+                PrintPeople(results);
+            }
+        }
+
+        static void SearchByLastName(List<Person> people, string lastName)
+        {
+            var results = people.Where(p => p._LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine($"No matching records found for last name: {lastName}");
+            }
+            else
+            {
+                Console.WriteLine($"\nSearch Results for Last Name: {lastName}");
+                PrintPeople(results);
+            }
+        }
+
+        static void SearchByOccupation(List<Person> people, string occupation)
+        {
+            var results = people.Where(p => p._Occupation.Contains(occupation, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine($"No matching records found for occupation: {occupation}");
+            }
+            else
+            {
+                Console.WriteLine($"\nSearch Results for Occupation: {occupation}");
+                PrintPeople(results);
+            }
+        }
+
+        static void ExportToCSV(List<Person> people)
+        {
+            Console.Write("Enter the CSV file name to export: ");
+            string fileName = Console.ReadLine();
+            fileName += ".csv";
+
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(fileName))
+            {
+                // Write CSV headers
+                file.WriteLine("firstname,lastname,occupation,age");
+
+                // Write data for each person
+                foreach (Person p in people)
+                {
+                    file.WriteLine($"{p._FirstName},{p._LastName},{p._Occupation},{p._Age}");
+                }
+            }
+
+            Console.WriteLine($"Data exported to {fileName}");
+        }
+
+        static int GetChoice(int min, int max)
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < min || choice > max)
+            {
+                Console.WriteLine("Invalid choice. Please enter a valid option.");
+                                Console.Write("Enter your choice: ");
+            }
+            return choice;
         }
     }
 
@@ -164,3 +301,4 @@ namespace OOP
         }
     }
 }
+
